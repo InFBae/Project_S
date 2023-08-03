a9cc1fa7107172b5311a83fccf7be15609042fd7
YongJoon <be6436@naver.com>
2023-08-03 19:48:47 +0900

    feat. Add Move and Jump

---
+++ Assets/YongJoon/Scripts/PlayerMover.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float nomalSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private Camera cam;
+
+    private Rigidbody rb;
+    private Vector3 moveDir;
+    private Vector3 moveVec;
+    private Vector3 dir;
+    private float curSpeed;
+    private bool isWalk = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        // ºÎµúÇûÀ» ‹š È¸Àü ¹æÁö
+        rb.freezeRotation = true;
+    }
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        dir = cam.transform.localRotation * moveDir;
+        dir = new Vector3(dir.x, 0f, dir.z);
+
+        curSpeed = isWalk ? walkSpeed : nomalSpeed;
+
+        moveVec = dir * curSpeed;
+        rb.velocity = moveVec + Vector3.up * rb.velocity.y;
+    }
+    public void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit hit;
+        return Physics.SphereCast(transform.position + Vector3.up * 1f,
+            0.5f, Vector3.down, out hit, 0.6f);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        moveDir = new Vector3(input.x, 0, input.y);
+    }
+    private void OnWalk(InputValue value)
+    {
+        isWalk = value.isPressed ? true : false;
+    }
+    private void OnJump(InputValue value)
+    {
+        if (IsGrounded())
+        {
+            Jump();
+        }
+    }
+}
