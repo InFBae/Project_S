20a83a3a187c94da6d9dcc77823529f2c6a9c207
YongJoon <be6436@naver.com>
2023-08-07 19:02:15 +0900

    feat.Camera Stack

--- Assets/YongJoon/Scripts/PlayerAimMove.cs
+++ Assets/YongJoon/Scripts/PlayerAimMove.cs
@@ -16,10 +16,13 @@ public class PlayerAimMove : MonoBehaviour
     private Vector2 lookDelta;
     private float xRotation;
     private float yRotation;
+    Vector3 upTrans;
+
 
     private void Awake()
     {
         curRoot = cameraRoot;
+        upTrans = new Vector3(0, 1.7f, 0);
     }
 
     private void Update()
@@ -35,7 +38,8 @@ public class PlayerAimMove : MonoBehaviour
         // 보고있는 물체의 위치
         Vector3 lookPoint = Camera.main.transform.position + Camera.main.transform.forward * lookDistance;
         realAimTarget.position = lookPoint;
-        playerAimTarget.position = new Vector3(playerAimTarget.position.x, lookPoint.y, playerAimTarget.position.z);
+        //new Vector3(playerAimTarget.position.x, lookPoint.y, playerAimTarget.position.z);
+        playerAimTarget.position = realAimTarget.position;
         // 보고있는 방향의 수직값은 현재의 수직값 
         lookPoint.y = transform.position.y;
         // 룩포인트 보기
@@ -51,6 +55,7 @@ public class PlayerAimMove : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
         // 카메라 루트를 회전하는식으로
         cameraRoot.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        cameraRoot.position = transform.position + upTrans;
     }
 
     private void OnAim(InputValue value)
