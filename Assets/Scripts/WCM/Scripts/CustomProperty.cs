612f5a47b05a148112a88b0742afe37f7483c3fc
우창민 <rebuilding33@gmail.com>
2023-08-11 21:43:55 +0900

    feat : wcm내의 script작업

---
+++ Assets/Scripts/WCM/Scripts/CustomProperty.cs
@@ -0,0 +1,114 @@
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using PhotonHashtable = ExitGames.Client.Photon.Hashtable;
+
+// 커스텀 프로퍼티를 확장메서드로 사용
+public static class CustomProperty
+{
+    public const string READY = "Ready";
+    public const string LOAD = "Load";
+
+    public const string LOADTIME = "LoadTime";
+
+    public static bool GetReady(this Player player)
+    {
+        PhotonHashtable property = player.CustomProperties;
+        if (property.ContainsKey("Ready"))
+            return (bool)property["Ready"];
+        else
+            return false;
+    }
+
+    public static void SetReady(this Player player, bool ready)
+    {
+        PhotonHashtable property = new PhotonHashtable();
+        property["Ready"] = ready;
+        player.SetCustomProperties(property);
+    }
+
+    public static bool GetLoad(this Player player)
+    {
+        PhotonHashtable property = player.CustomProperties;
+        if (property.ContainsKey("Load"))
+            return (bool)property["Load"];
+        else
+            return false;
+    }
+
+    public static void SetNick(this Player player, string nick)
+    {
+        PhotonHashtable property = new PhotonHashtable();
+        property["Nickname"] = nick;
+        player.SetCustomProperties(property);
+    }
+
+    public static string GetNick(this Player player)
+    {
+        PhotonHashtable property = player.CustomProperties;
+        if (property.ContainsKey("Nick"))
+            return (string)property["Nick"];
+        else
+            return "";
+    }
+
+    public static void SetLoad(this Player player, bool load)
+    {
+        PhotonHashtable property = new PhotonHashtable();
+        property["Load"] = load;
+        player.SetCustomProperties(property);
+    }
+
+    public static int GetLoadTime(this Room room)
+    {
+        PhotonHashtable property = room.CustomProperties;
+        if (property.ContainsKey("LoadTime"))
+            return (int)property["LoadTime"];
+        else
+            return -1;
+    }
+
+    public static void SetLoadTime(this Room room, int loadTime)
+    {
+        PhotonHashtable property = new PhotonHashtable();
+        property["LoadTime"] = loadTime;
+        room.SetCustomProperties(property);
+    }
+
+    //room customproperty 받아 오는 법
+    public static float GetRoomInfo_maxKill(this Room room)
+    {
+        PhotonHashtable property = room.CustomProperties;
+        if (property.ContainsKey("maxKill"))
+            return (int)property["maxKill"];
+        else
+            return -1;
+    }
+    public static string GetRoomInfo_gameTime(this Room room)
+    {
+        PhotonHashtable property = room.CustomProperties;
+        if (property.ContainsKey("gameTime"))
+            return (string)property["gameTime"];
+        else
+            return "";
+    }
+    public static bool GetRoomInfo_canIrrupt(this Room room)
+    {
+        PhotonHashtable property = room.CustomProperties;
+        if (property.ContainsKey("CanIrrupt"))
+            return (bool)property["CanIrrupt"];
+        else
+            return false;
+    }
+    /*public static bool GetRoomInfo_IsPlayingNow(this Room room)
+    {
+        PhotonHashtable property = room.CustomProperties;
+        if (property.ContainsKey("IsPlayingNow"))
+            return (bool)property["IsPlayingNow"];
+        else
+            return false;
+    }*/
+
+}
---
+++ Assets/Scripts/WCM/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,126 @@
+using Cinemachine;
+using Photon.Pun;
+using Photon.Pun.Demo.Asteroids;
+using Photon.Pun.UtilityScripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviourPun, IPunObservable
+{
+    [SerializeField] List<Color> playerColor;
+    [SerializeField] float movePower;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] Bullet bulletPrefab;
+    [SerializeField] float fireCoolTime;
+
+    private PlayerInput playerInput;
+    private Rigidbody rigid;
+    private CinemachineVirtualCamera cm;
+    private Vector2 inputDir;
+    private float lastFireTime = float.MinValue;
+
+    [SerializeField] int fireCount;
+    [SerializeField] float moveSpeed;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        rigid = GetComponent<Rigidbody>();
+        cm = FindObjectOfType<CinemachineVirtualCamera>();
+
+        SetPlayerColor();
+
+        if (photonView.IsMine)
+        {
+            cm.Follow = transform;
+            cm.LookAt = transform;
+        }
+        else
+        {
+            Destroy(playerInput);
+        }
+    }
+
+    private void Update()
+    {
+        Accelate(inputDir.y);
+        Rotate(inputDir.x);
+
+        moveSpeed = rigid.velocity.magnitude;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        inputDir = value.Get<Vector2>();
+    }
+
+    private void OnFire(InputValue value)
+    {
+        if (value.isPressed)
+            Fire();
+    }
+
+    private void Accelate(float input)
+    {
+        rigid.AddForce(input * movePower * transform.forward, ForceMode.Force);
+        if (rigid.velocity.magnitude > maxSpeed)
+        {
+            rigid.velocity = rigid.velocity.normalized * maxSpeed;
+        }
+    }
+
+    private void Rotate(float input)
+    {
+        transform.Rotate(Vector3.up, input * rotateSpeed * Time.deltaTime);
+    }
+
+    private void Fire()
+    {
+        photonView.RPC("RequestCreateBullet", RpcTarget.MasterClient);
+    }
+
+    [PunRPC]
+    public void RequestCreateBullet()
+    {
+        if (Time.time < lastFireTime + fireCoolTime)
+            return;
+
+        lastFireTime = Time.time;
+        photonView.RPC("ResultCreateBullet", RpcTarget.AllViaServer, transform.position, transform.rotation);
+    }
+
+    [PunRPC]
+    public void ResultCreateBullet(Vector3 position, Quaternion rotation, PhotonMessageInfo info)
+    {
+        float lag = (float)(PhotonNetwork.Time - info.SentServerTime);
+
+        Bullet bullet = Instantiate(bulletPrefab);
+        //bullet.Init(position, rotation, lag);
+        fireCount++;
+    }
+
+    private void SetPlayerColor()
+    {
+        int playerNumber = photonView.Owner.GetPlayerNumber();
+        if (playerColor == null || playerColor.Count <= playerNumber)
+            return;
+
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = playerColor[playerNumber];
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(moveSpeed);
+        }
+        else // stream.IsReading
+        {
+            moveSpeed = (float)stream.ReceiveNext();
+        }
+    }
+}
