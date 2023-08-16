c5cc400957157fb8bb8a1ddf18f04916d6d74075
JUNG BOBAE <acarid27@gmail.com>
2023-08-16 21:43:34 +0900

    fix: Fix DiePlayerPrefab

--- Assets/ADB/Scripts/RE/ADB_RE_DiePlayer.cs
+++ Assets/ADB/Scripts/RE/ADB_RE_DiePlayer.cs
@@ -6,18 +6,13 @@ public class ADB_RE_DiePlayer : MonoBehaviour
 {
     // Player의 transform와 계속 똑같아야 함
 
-    [SerializeField] Transform Player;
-
     private void Awake()
     {
         //gameObject.SetActive(false);
-
     }
 
     private void OnEnable()
     {
-        gameObject.transform.position = Player.transform.position;
-        gameObject.transform.rotation = Player.transform.rotation;
         Destroy(gameObject.transform.parent.gameObject, 5f);
     }
 
