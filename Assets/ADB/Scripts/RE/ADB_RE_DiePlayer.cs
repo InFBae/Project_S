bdf6b4a4d5e166decdf3ac472fd96f8ef7bc1279
JUNG BOBAE <acarid27@gmail.com>
2023-08-16 21:46:00 +0900

    fix: Fix DiePlayer

--- Assets/ADB/Scripts/RE/ADB_RE_DiePlayer.cs
+++ Assets/ADB/Scripts/RE/ADB_RE_DiePlayer.cs
@@ -1,8 +1,9 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ADB_RE_DiePlayer : MonoBehaviour
+public class ADB_RE_DiePlayer : MonoBehaviourPun
 {
     // Player의 transform와 계속 똑같아야 함
 
